688ae4ae6464eb816d967d06cb5af6b7e785d9e2
Daniil_Khobotov <dinich44@gmail.com>
2022-10-27 14:01:25 +0400

    add test

--- src/js/main.js
+++ src/js/main.js
@@ -1,49 +1,49 @@
 import $ from './lib/lib';
 
-$('#first').on('click', () => {
-    $('div').eq(1).fadeToggle(800);
-});
+// $('#first').on('click', () => {
+//     $('div').eq(1).fadeToggle(800);
+// });
 
-$('[data-count="second"]').on('click', () => {
-    $('div').eq(2).fadeToggle(800);
-});
+// $('[data-count="second"]').on('click', () => {
+//     $('div').eq(2).fadeToggle(800);
+// });
 
-$('button').eq(2).on('click', () => {
-    $('.w-500').fadeToggle(800);
-});
+// $('button').eq(2).on('click', () => {
+//     $('.w-500').fadeToggle(800);
+// });
 
-$('#trigger').click(() => $('#trigger').createModal({
-    text: {
-        title: 'Modal title',
-        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo voluptatibus ex odit minus esse reprehenderit, excepturi, nihil exercitationem non est enim reiciendis voluptatem culpa expedita a suscipit possimus corporis blanditiis.',
-    },
-    btns: {
-        count: 3,
-        settings: [
-            [
-                'Close',
-                ['btn-danger', 'mr-10'],
-                true,
-            ],
-            [
-                'Save changes',
-                ['btn-success'],
-                false,
-                () => {
-                    alert('Данные сохранены');
-                }
-            ],
-            [
-                'Another btn',
-                ['btn-warning', 'ml-10'],
-                false,
-                () => {
-                    alert('Hello World');
-                }
-            ]
-        ]
-    }
-}));
+// $('#trigger').click(() => $('#trigger').createModal({
+//     text: {
+//         title: 'Modal title',
+//         body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo voluptatibus ex odit minus esse reprehenderit, excepturi, nihil exercitationem non est enim reiciendis voluptatem culpa expedita a suscipit possimus corporis blanditiis.',
+//     },
+//     btns: {
+//         count: 3,
+//         settings: [
+//             [
+//                 'Close',
+//                 ['btn-danger', 'mr-10'],
+//                 true,
+//             ],
+//             [
+//                 'Save changes',
+//                 ['btn-success'],
+//                 false,
+//                 () => {
+//                     alert('Данные сохранены');
+//                 }
+//             ],
+//             [
+//                 'Another btn',
+//                 ['btn-warning', 'ml-10'],
+//                 false,
+//                 () => {
+//                     alert('Hello World');
+//                 }
+//             ]
+//         ]
+//     }
+// }));
 
 // $().get('https://jsonplaceholder.typicode.com/todos/1')
 //     .then(res => console.log(res));
